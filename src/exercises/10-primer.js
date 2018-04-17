90766a6470f2ff6fd8109ecb2819f7ec98789b79
tusharc88 <tushar54983@gmail.com>
2018-04-17 14:14:16 -0700

    Adding 10-primer

--- src/exercises/10-primer.js
+++ src/exercises/10-primer.js
@@ -13,25 +13,36 @@ class Toggle extends React.Component {
   // the on prop is controlled. Call it `isOnControlled`.
   // 💰 this.props.on !== undefined
   //
+  isOnControlled = () => {
+    return this.props.on !== undefined
+  }
   // 🐨 Now let's add a function that can return the state
   // whether it's coming from this.state or this.props
   // Call it `getState` and have it return on from
   // state if it's not controlled or props if it is.
+  getState() {
+    return {
+      on: this.isOnControlled() ? this.props.on : this.state.on,
+    }
+  }
   toggle = () => {
     // 🐨 if the toggle is controlled, then we shouldn't
     // be updating state. Instead we should just call
     // `this.props.onToggle` with what the state should be
+    if (this.isOnControlled()) {
+      this.props.onToggle(!this.getState().on)
+    }
     this.setState(
       ({on}) => ({on: !on}),
       () => {
-        this.props.onToggle(this.state.on)
+        this.props.onToggle(this.getState().on)
       },
     )
   }
   render() {
     // 🐨 rather than getting state from this.state,
     // let's use our `getState` method.
-    const {on} = this.state
+    const {on} = this.getState()
     return <Switch on={on} onClick={this.toggle} />
   }
 }
