d645f04c2802223529e8be9b3ce071c47c767324
tusharc88 <tushar54983@gmail.com>
2018-04-17 12:20:00 -0700

    Doing exercise 06 and 07

--- src/exercises/06.js
+++ src/exercises/06.js
@@ -22,14 +22,22 @@ class Toggle extends React.Component {
       ({on}) => ({on: !on}),
       () => this.props.onToggle(this.state.on),
     )
+
+  getTogglerProps = ({onClick, ...props}) => {
+    return {
+      onClick: (...args) => {
+        onClick && onClick(...args)
+        this.toggle()
+      },
+      ...props,
+    }
+  }
+
   getStateAndHelpers() {
     return {
       on: this.state.on,
       toggle: this.toggle,
-      togglerProps: {
-        'aria-expanded': this.state.on,
-        onClick: this.toggle,
-      },
+      getTogglerProps: this.getTogglerProps,
     }
   }
   render() {
