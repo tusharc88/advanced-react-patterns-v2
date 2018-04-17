d63ca9efac3d2bf7fca3e1e23b991841d9ff9f69
tusharc88 <tushar54983@gmail.com>
2018-04-17 09:41:42 -0700

    email autofill

--- src/__tests__/09.js
+++ src/__tests__/09.js
@@ -91,7 +91,7 @@ test('can click too much', () => {
 // 3. Change submitted from `false` to `true`
 // 4. And you're all done!
 /*
-http://ws.kcd.im/?ws=react%20patterns&e=09&em=
+http://ws.kcd.im/?ws=react%20patterns&e=09&em=tushar54983@gmail.com
 */
 test.skip('I submitted my elaboration and feedback', () => {
   const submitted = false // change this when you've submitted!
